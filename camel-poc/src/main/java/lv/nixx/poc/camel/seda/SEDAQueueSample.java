ea9be73c6f7791a15a9e23070204aebc08fe52ed
nixx78 <nixx78@gmail.com>
2017-01-31 18:09:16 +0200

    SEDA queue usage sample added

---
+++ camel-poc/src/main/java/lv/nixx/poc/camel/seda/SEDAQueueSample.java
@@ -0,0 +1,45 @@
+package lv.nixx.poc.camel.seda;
+
+import org.apache.camel.Exchange;
+import org.apache.camel.Processor;
+import org.apache.camel.builder.RouteBuilder;
+
+import lv.nixx.poc.camel.common.CamelRunner;
+
+public class SEDAQueueSample {
+
+	public static void main(String args[]) throws Exception {
+		
+		CamelRunner runner = new CamelRunner();
+		runner.main().addRouteBuilder(new RouteBuilder() {
+			public void configure() {
+	            from("timer:simpleTimer?period=5000").to("seda:queue?multipleConsumers=true").end();
+	            
+	            from("seda:queue?multipleConsumers=true")
+		            .routeId("firstRoute")
+		            .process(new Processor() {
+						@Override
+						public void process(Exchange exchange) throws Exception {
+			            	System.out.println("--- BeanOne is called");
+						}
+		            })
+		            .end();
+
+	            from("seda:queue?multipleConsumers=true")
+	            .routeId("secondRoute")
+	            .process(new Processor() {
+					@Override
+					public void process(Exchange exchange) throws Exception {
+		            	System.out.println("+++ BeanTwo is called");
+					}
+	            })
+	            .end();
+	            
+			}
+		});
+		
+		runner.run();
+	}
+
+
+}
